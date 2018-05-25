8c9fefa52a6a5900de8fe8c43fd9c6cba46c864d
leoiphonedev <aggarwalaman1985@gmail.com>
2018-05-25 15:15:30 +0530

    Initial Commit

---
+++ Add delete cell UItableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Add delete cell UItableView
+//
+//  Created by Aman Aggarwal on 25/05/18.
+//  Copyright © 2018 Aman Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
